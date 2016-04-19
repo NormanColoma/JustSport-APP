1dfe2cf109b9e25327a97f92239f63673deb56f3
Norman <ua.norman@gmail.com>
2016-04-19 21:50:32 +0200

    Adding protractor options for Travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,8 +2,6 @@
  * Created by Norman on 03/12/2015.
  */
 exports.config = {
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
     allScriptsTimeout: 11000,
 
     specs: [
@@ -12,7 +10,6 @@ exports.config = {
 
     capabilities: {
         'browserName': 'chrome',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER
     },
 
