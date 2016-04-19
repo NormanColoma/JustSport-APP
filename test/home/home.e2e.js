a16c97117b1eb385570c862c9931e3285ea2bb59
Norman <ua.norman@gmail.com>
2016-04-19 18:34:46 +0200

    E2E: Register. OK

--- test/home/home.e2e.js
+++ test/home/home.e2e.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var app = express();
 
-describe('Home', function() {
+xdescribe('Home', function() {
     var host = 'http://localhost:5000';
     if(app.get('env') === 'test'){
         host = 'https://localhost:5000';
