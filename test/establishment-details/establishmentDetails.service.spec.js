7f91f0825b31545e15466981d658daa44947762f
Norman <ua.norman@gmail.com>
2016-05-11 11:57:32 +0200

    TDD: Establishment Details Service. Get Establishment. FAIL

---
+++ test/establishment-details/establishmentDetails.service.spec.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Norman on 11/05/2016.
+ */
+describe('Establishment Details Service that retrieves full establishment', function() {
+    var $httpBackend;
+    var baseAPI = 'https://localhost:3000/api/';
+    var estService;
+    // Set up the module
+    beforeEach(module('justSport'));
+
+    beforeEach(inject(function ($injector, establishmentDetailsService) {
+        // Set up the mock http service responses
+        $httpBackend = $injector.get('$httpBackend');
+        estService = establishmentDetailsService;
+    }));
+
+    it('Should return the full establishment when passsing its id',function(){
+        var estab = {id: 1,name: "Nombre Gym", desc: "Esta es la desc",city: "Alicante",
+        province: "Alicante", addr: "Esta es la dirección", phone: "965660427", website: "www.pagina.com",
+        main_img:"default.jpg", Votes:[{user: "8a74a3aa-757d-46f1-ba86-a56a0f107735"}],
+        Commentaries:[{id: 1,
+            text: "El gimnasio tiene unas instalaciones increíbles",
+            createdAt: "2016-05-11T09:02:45.000Z"}]
+        };
+        var est = null;
+        $httpBackend.expectGET(baseAPI+'establishments/1').respond(estab);
+        estService.getEstablishment(1).then(function(data){
+            est = data;
+        });
+        $httpBackend.flush();
+        var expc_est = estab;
+        expect(est).toEqual(expc_est);
+    });
+});
