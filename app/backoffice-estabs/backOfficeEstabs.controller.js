a94689bf3b7c7f0be1c7899596b981c753f40f17
Norman <ua.norman@gmail.com>
2016-05-31 20:30:40 +0200

    E2E test pending

--- app/backoffice-estabs/backOfficeEstabs.controller.js
+++ app/backoffice-estabs/backOfficeEstabs.controller.js
@@ -9,7 +9,7 @@ angular
     .module('backOfficeModule')
     .controller('BackOfficeEstabController', BackOfficeEstabController);
 
-    BackOfficeEstabController.$inject = ['backOfficeEstabService', 'dialogService', 'loginService','citySuggestionsService'];
+    BackOfficeEstabController.$inject = ['backOfficeEstabService', 'dialogService', 'loginService','citySuggestionsService','formResetService'];
 
     /**
     *
@@ -18,7 +18,7 @@ angular
     * @memberOf BackOffice Estabs
     *
     */
-    function BackOfficeEstabController(backOfficeEstabService, dialogService, loginService, citySuggestionsService) {
+    function BackOfficeEstabController(backOfficeEstabService, dialogService, loginService, citySuggestionsService,formResetService) {
         var vm = this;
 
         var local_folder = "https://localhost:3000/";
@@ -28,11 +28,9 @@ angular
         vm.addEstablishment = addEstablishment;
         vm.after = "none";
         vm.changeView = changeView;
-        vm.city = "";
         vm.estabs = [];
         vm.getEstabs = getEstabs;
         vm.imgFolder = server+"public/images/ests/";
-        vm.province = "";
         vm.querySearch = querySearch;
         vm.searchCityChange = searchCityChange;
         vm.searchProvinceChange = searchProvinceChange;
@@ -48,16 +46,32 @@ angular
          * @memberOf BackOffice Estabs.BackOfficeEstabController
          * @returns {void}
          */
-        function addEstablishment(data,ev){
-            if(vm.estab !== undefined){
-                backOfficeEstabService.addEstablishment(data).then(function(data){
-                    if(data.message === undefined){
-                        vm.estabs.push(data);
-                    }else{
-
-                    }
-                });
-            }
+        function addEstablishment(data,ev, form){
+            vm.estab.owner = localStorage.user_id;
+            vm.estab.main_img = 'default.jpg';
+            console.log(vm.estab);
+            var dataDialog = {};
+            backOfficeEstabService.addEstablishment(data).then(function(data){
+                if(data.message === undefined){
+                    vm.estabs.push(data);
+                    formResetService.reset(form);
+                    vm.estab = {};
+                    vm.searchText = null;
+                    vm.searchTextProvince = null;
+                    dataDialog = {
+                        title: '¡Establecimiento Añadido!', text: 'El establecimiento ha sido añadido correctamente.',
+                        aria: 'Estab Added Alert', textButton: 'Listo'
+                    };
+                    changeView('listEstabs');
+                    dialogService.showDialog(dataDialog, ev);
+                }else{
+                    dataDialog = {
+                        title: '¡Erro al Añadir!', text: 'El establecimiento no se ha podido añadir. ' +
+                        'Por favor, vuelve a intentar añadir el mismo.',
+                        aria: 'Estab Added Alert Failed', textButton: 'Listo'
+                    };
+                }
+            });
         };
 
         function changeView(view){
@@ -75,10 +89,18 @@ angular
             if(loginService.isLoggedIn()) {
                 backOfficeEstabService.getEstabs(vm.after).then(angular.bind(this, function (data) {
                     vm.after = data.paging.cursors.after;
+                    for(var i=0;i<data.Establishments.length;i++){
+                        for(var j=0;j<vm.estabs.length;j++){
+                            if(vm.estabs[j].name === data.Establishments[i].name){
+                                data.Establishments.splice(i,1);
+                            }
+                        }
+                    }
                     vm.estabs = vm.estabs.concat(data.Establishments);
                 }));
             }
         }
+
 
         function querySearch(query){
             return citySuggestionsService.getCities(query).then(function(data){
@@ -87,11 +109,11 @@ angular
         }
 
         function searchCityChange(city){
-            vm.city = city;
+            vm.estab.city = city;
         }
 
         function searchProvinceChange(province){
-            vm.province = province;
+            vm.estab.province = province;
         }
 
     }
