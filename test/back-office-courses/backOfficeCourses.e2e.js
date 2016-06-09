97338313d0c56a9440c939e33807d308f80d5399
Norman <ua.norman@gmail.com>
2016-06-09 18:27:47 +0200

    E2E test were added

--- test/back-office-courses/backOfficeCourses.e2e.js
+++ test/back-office-courses/backOfficeCourses.e2e.js
@@ -44,10 +44,36 @@ describe('Courses Backoffice', function() {
             expect(l_courses.length).toBe(3);
             l_courses[1].click();
         });
-
-        acc.click();
-        element(by.css('[name="logout"]')).click();
     });
 
+    it('Should show validations errors',function(){
+        var ins = element(by.css('[name="cins"]'));
+        var price = element(by.css('[name="cprice"]'));
+        var p_ins = element(by.css('[name="pins"]'));
+        var p_price = element(by.css('[name="pprice"]'));
+        ins.clear();
+        price.clear();
+        ins.sendKeys('Pepe12')
+        price.sendKeys('-1');
+        expect(p_ins.getText()).toEqual("El nombre del instructor no es correcto. Solo admite caracteres alfanuméricos.");
+        expect(p_price.getText()).toEqual("El precio del curso debe ser por lo menos 1.");
+    });
+
+    it('Should update the course correclty', function(){
+        var ins = element(by.css('[name="cins"]'));
+        var price = element(by.css('[name="cprice"]'));
+        var update_btn = element(by.id('update-course-btn'));
+        ins.clear();
+        price.clear();
+        ins.sendKeys('Pepe Castaño')
+        price.sendKeys('20.75');
+        update_btn.click();
+        var alert_content = element(by.css('md-dialog-content'));
+        var alert_text = alert_content.element(by.css('p')).getText();
+        expect(alert_text).toEqual('La información del curso ha sido actualizada.');
+        element(by.css('md-dialog-actions')).element(by.css('button')).click();
+        element(by.css('.account-options')).click();
+        element(by.css('[name="logout"]')).click();
+    });
 
 });
