1ef6e071c911f63f44ad1910a5f5ab402d478069
Norman <ua.norman@gmail.com>
2016-06-09 12:39:06 +0200

    TDD:Back Office Course Controller. Update Course. FAIL

--- test/back-office-courses/backOfficeCourses.controller.spec.js
+++ test/back-office-courses/backOfficeCourses.controller.spec.js
@@ -1,7 +1,7 @@
 /**
  * Created by Norman on 08/06/2016.
  */
-describe('Back Office Courses Controller', function() {
+fdescribe('Back Office Courses Controller', function() {
     var $httpBackend, $rootScope, createController;
     var baseAPI = 'https://localhost:3000/api/';
 
@@ -20,5 +20,42 @@ describe('Back Office Courses Controller', function() {
             return $controller('BackOfficeCoursesController', {'$scope': $rootScope});
         };
     }));
+    it('Should update the establishment correctly', function(){
+        var controller = createController();
+        localStorage.setItem('username','Norman');
+        var d = {info: "La información ha cambiado"};
+        $httpBackend.expectGET(baseAPI+'establishment/me/all?limit=200').respond(200);
+        $httpBackend.expectPUT(baseAPI+'courses/1').respond(204);
+        controller.courses = [
+            {establishmentId: 1, rows:[
+                {
+                    "id": 1,
+                    "info": "Un curso muy completo",
+                    "price": 17.5,
+                    "instructor": "Juan Domínguez",
+                },
+                {
+                    "id": 2,
+                    "info": "Un curso no tan completo",
+                    "price": 20,
+                    "instructor": "Pepe Castaño",
+                },
+                {
+                    "id": 3,
+                    "info": "Un curso poco completo",
+                    "price": 15,
+                    "instructor": "María Castro",
+                }
+            ]}
+        ]
+        ;
+        expect(controller.courses[0].rows.length).toBe(3);
+        /* jshint ignore:start*/
+        controller.updateCourse(1,d);
+        /*jshint ignore:end */
+        $httpBackend.flush();
+        expect(controller.courses[0].rows[0].info).toEqual("La información ha cambiado");
+        localStorage.removeItem('username');
+    });
 
 });
--- test/back-office-courses/backOfficeCourses.service.spec.js
+++ test/back-office-courses/backOfficeCourses.service.spec.js
@@ -1,7 +1,7 @@
 /**
  * Created by Norman on 08/06/2016.
  */
-fdescribe('Back Office Courses Service that handles courses', function() {
+describe('Back Office Courses Service that handles courses', function() {
     var $httpBackend;
     var baseAPI = 'https://localhost:3000/api/';
     var backOCService;
