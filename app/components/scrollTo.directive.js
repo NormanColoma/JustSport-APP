66d00ee6fdcce98ec469d5b015a74f53764fd8af
Norman <ua.norman@gmail.com>
2016-06-15 15:29:12 +0200

    responsive changes

--- app/components/scrollTo.directive.js
+++ app/components/scrollTo.directive.js
@@ -16,10 +16,13 @@ angular
         function link(scope, element, attrs) {
             var scroll = $('#scroll');
             var results = $('#results');
+            var results2 = $('#results2')
             var title = $('.est-title');
             element.on('click', function(){
                 if(attrs.id == "scrollResults"){
                     $('body').animate({scrollTop: results.offset().top}, "slow");
+                }else if(attrs.id == "scrollResults2"){
+                    $('body').animate({scrollTop: results2.offset().top}, "slow");
                 }else if(attrs.id == "update-estab" || attrs.id === "addEst" || attrs.id === "listEst" || attrs.id === "addCourse" || attrs.id === "listCourses"){
                     $('body').animate({scrollTop: $('body').offset().top}, "slow");
                 }else if(attrs.id == "getDetails"){
